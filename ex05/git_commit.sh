6b102b68b5ca56ba91111ea55d05c050d153862f
Eliam Detoh <edetoh@c1r7p2.42lehavre.fr>
2024-08-05 15:05:50 +0200

    ex05

--- ex05/git_commit.sh
+++ ex05/git_commit.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-commits=$(git log --pretty=format:"%H")
 compteur=0
 
-for commit in commits; do
-    if ((compteur <= 5)); then
+git log --pretty=format:"%H" | while read commit; do
+    if ((compteur < 5)); then
         echo $commit
         compteur=$((compteur + 1))
+    else
+        break
     fi
 done
