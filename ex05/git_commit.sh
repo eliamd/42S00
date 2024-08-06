17c75d3c8cd8f42815905d3cafba580e7e8c3f31
Eliam Detoh <edetoh@c1r7p2.42lehavre.fr>
2024-08-06 10:45:23 +0200

    corrige

--- ex05/git_commit.sh
+++ ex05/git_commit.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-compteur=0
-
-git log --pretty=format:"%H" | while read commit; do
-    if ((compteur < 5)); then
-        echo $commit
-        compteur=$((compteur + 1))
-    else
-        break
-    fi
-done
+git log -n5 --pretty=format:"%H"
